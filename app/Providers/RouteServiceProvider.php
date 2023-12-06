f5d6bdf7b16182ff5e44a4af14bcd2492e1b848b
nathan-dykstra <nathan.dykstra@gmail.com>
2023-12-06 00:22:52 -0500

    Updated routing

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,17 +24,87 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        $this->configureRateLimiting();
 
         $this->routes(function () {
+            // Load all route files from specified folders recursively
+
+            // Can be accessed by any admin or user without a specific role
+            foreach ($this->phpFilesInFolder(base_path('/routes/web')) as $route_file) {
+                $this->addWebRoutes($route_file, ['auth']);
+            }
+
+            foreach ($this->phpFilesInFolder(base_path('/routes/api')) as $route_file) {
+                $this->addApiRoutes($route_file, ['auth:sanctum']);
+            }
+
+            // ---------------------------------------------------------------------
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
         });
+    }
+
+    // ---------------------------------------------------------------------
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+    }
+
+    protected function addWebRoutes(string $route_file_path, array $middleware = []): void
+    {
+        $middleware = array_unique(array_merge(['web'], $middleware));
+        Route::middleware($middleware)->group($route_file_path);
+    }
+
+    protected function addApiRoutes(string $route_file_path, array $middleware = []): void
+    {
+        Route::prefix('api')
+            ->middleware(array_merge(['api'], $middleware))
+            ->group($route_file_path);
+    }
+
+    // ---------------------------------------------------------------------
+
+    protected function phpFilesInFolder(string $rootPath, array &$route_file_paths = []): array
+    {
+        $files = scandir($rootPath);
+
+        foreach ($files as $filename) {
+            if (! $this->isSpecialDirectory($filename)) {
+
+                $path = $this->absolutePath($rootPath, $filename);
+
+                if (is_dir($path)) {
+                    $this->phpFilesInFolder($path, $route_file_paths);
+                } else {
+                    if ($this->isPHPFile($filename)) {
+                        $route_file_paths[] = $path;
+                    }
+                }
+            }
+        }
+
+        return $route_file_paths;
+    }
+
+    protected function absolutePath(string $root_path, string $filename): string
+    {
+        return (string) realpath($root_path.DIRECTORY_SEPARATOR.$filename);
+    }
+
+    protected function isPHPFile(string $filename): bool
+    {
+        return pathinfo($filename, PATHINFO_EXTENSION) == 'php';
+    }
+
+    protected function isSpecialDirectory(string $filename): bool
+    {
+        return in_array($filename, ['.', '..']);
     }
 }
