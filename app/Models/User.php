3758328e1807c2b35a4ddf998c30cc5ad70c808c
nathan-dykstra <nathan.dykstra@gmail.com>
2024-05-20 22:07:50 -0400

    Cleanup: remove intervention/image

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Log;
-use Intervention\Image\Facades\Image;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
