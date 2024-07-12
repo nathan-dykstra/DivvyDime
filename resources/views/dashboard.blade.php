65261683e8ae2408c390c196f6f36a43adbe3473
nathan-dykstra <nathan.dykstra@gmail.com>
2024-07-12 18:24:02 -0400

    Added "Add expense" and "Settle up" buttons to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,5 +5,21 @@
         {{ $greeting . ', ' . $current_user->username }}
     </x-slot>
 
+    <x-slot name="header_buttons">
+        <x-primary-button icon="fa-solid fa-receipt icon" :href="route('expenses.create')">{{ __('Add Expense') }}</x-primary-button>
+        <x-primary-button icon="fa-solid fa-scale-balanced icon" :href="route('payments.create')">{{ __('Settle Up') }}</x-primary-button>
+    </x-slot>
+
+    <x-slot name="mobile_overflow_options">
+        <a class="dropdown-item" href="{{ route('expenses.create') }}">
+            <i class="fa-solid fa-receipt"></i>
+            <div>{{ __('Add Expense') }}</div>
+        </a>
+        <a class="dropdown-item" href="{{ route('payments.create') }}">
+            <i class="fa-solid fa-scale-balanced"></i>
+            <div>{{ __('Settle Up') }}</div>
+        </a>
+    </x-slot>
+
     <p>You're logged in! More features coming to this page soon...</p>
 </x-app-layout>
